77ed9a6583853f1816693ee39f19cb03f9f23d0d
gizmo126 <samghuang@users.noreply.github.com>
2018-03-08 00:33:38 -0600

    figuring out heroku

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // This is added so Heroku stops complaining
+/*
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(__dirname + '/dist/'));
+app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+
+
+app.listen(process.env.PORT || 8080);
+/*
 /*
 var express = require('express');
 var app     = express();
