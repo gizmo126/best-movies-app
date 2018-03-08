91b8b475d62628518aa8ece83d3685f040701ac2
gizmo126 <samghuang@users.noreply.github.com>
2018-03-08 04:38:32 -0600

    update server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname +'/source'));
+app.use(express.static(__dirname + '/source'));
 app.set('port', process.env.PORT || 8082);
 
-app.get('/server', (req, res) =>{
-  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'source/html/index.html'));
 });
 
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
     console.log('listening on port ', server.address().port);
 });
 /*
