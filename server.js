0195018a308e920219e822cd62697904a511cb5c
gizmo126 <samghuang@users.noreply.github.com>
2018-03-08 06:09:49 -0600

    successful heroku deployment

--- server.js
+++ server.js
@@ -2,19 +2,19 @@
 
 var path = require('path');
 var express = require('express');
-
 var app = express();
 
-app.use('/public', express.static('/public'))
-app.use('/source', express.static('/source'))
-app.set('port', process.env.PORT || 3000);
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(app.get('port'), function () {
-    console.log('listening on port ', server.address().port);
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 /*
