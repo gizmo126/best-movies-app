fc91a753bb63f224efd3eb95af803319006a6fc5
gizmo126 <samghuang@users.noreply.github.com>
2018-03-08 01:31:19 -0600

    trying something

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 // This is added so Heroku stops complaining
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require('express');
+var app     = express();
 
+app.set('port', (process.env.PORT || 5000));
 
-// Since the root/src dir contains our index.html
-app.use(express.static(__dirname + '/source/'));
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/source/html/index.html'));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-
-// Heroku bydefault set an ENV variable called PORT=443
-//  so that you can access your site with https default port.
-// Falback port will be 8080; basically for pre-production test in localhost
-// You will use $ npm run prod for this
-app.listen(process.env.PORT || 8081);
 
 /*
 var express = require('express');
