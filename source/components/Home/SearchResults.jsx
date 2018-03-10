e07b6fb697e0adafdf3092b37e464b39d29987f4
gizmo126 <samghuang@gmail.com>
2018-03-10 02:35:58 -0600

    updated key

--- source/components/Home/SearchResults.jsx
+++ source/components/Home/SearchResults.jsx
@@ -71,7 +71,7 @@ export class SearchResults extends Component {
 }
 
 function getResults(pages) {
-   let newUrl = 'movie/top_rated?api_key=' + TMDB_KEY;
+   let newUrl = 'movie/top_rated?api_key=ff9f06c5d8a831c7a2a99295c33936b7&language=en-US';
    if (pages >= 0) {
       let promises = [];
       for (let i = 1; i < 20; i++) {
